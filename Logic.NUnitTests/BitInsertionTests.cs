2886b032a358865f8fc63de069beb65d89214f0d
Christina <christina.golovach@gmail.com>
2018-10-15 18:10:00 +0300

    Add new test case in NUnit project

--- Logic.NUnitTests/BitInsertionTests.cs
+++ Logic.NUnitTests/BitInsertionTests.cs
@@ -8,8 +8,10 @@ namespace Logic.NUnitTests
     public class BitInsertionTests
     {
         [TestCase(8, 15, 3, 8, 120)]
-        [TestCase(int.MaxValue, -1, 31, 31, -1)]
+        [TestCase(15, 15, 0, 0, 15)]
         [TestCase(8, 8, 0, 31, 8)]
+        [TestCase(int.MaxValue, -1, 31, 31, -1)]            
+        [TestCase(-1, 0, 0, 30, int.MinValue)]
         public void InsertNumber_InsertingBitsOfNumberWithCorrectPositionOfThem_ReturnedNewNumber(int numberSource, int numberIn, int smallerBitNumber, int largeBitNumber, int result)
         {
             // Act
